6c2d3fc3e8320f28ee9300344ada2c27ae3732cf
luke.b.underwood@gmail.com <luke.b.underwood@gmail.com>
2020-01-24 09:07:14 -0900

    Labs 01 and 02 complete

--- Labs/Lab01/Main.cpp
+++ Labs/Lab01/Main.cpp
@@ -21,9 +21,18 @@ bool getInt(int& num)
 		return false;
 }
 
+void printList(std::vector<std::string> fav, std::vector<int> numbers, int counter)
+{
+	std::cout << "We will order:\n";
+	for (int i = 0; i < counter - 2; i++)
+	{
+		std::cout << numbers[i] << " " << fav[i] << " jump suits" << std::endl;
+	}
+}
+
 int main()
 {
-	std::cout << "Please enter some of your favorite things. Enter \"END\" when done.\n";
+	std::cout << "Please enter some of your favorite colors. Enter \"END\" when done.\n";
 	int counter = 1;
 	std::vector<std::string> fav;
 	std::string item;
@@ -35,15 +44,15 @@ int main()
 		fav.push_back(item);
 		counter++;
 	} while (item != "END");
+
 	fav.pop_back();
 
-	std::cout << "Please enter three integers.\n";
+	std::cout << "Please enter some numbers.\n";
 	std::vector<int> numbers;
 	int num;
 
-	for (int i = 0; i < 3; i++)
+	for (auto n : fav)
 	{
-		std::cout << i + 1 << ": ";
 		while (!getInt(num))
 		{
 			std::cout << "Please enter an integer: ";
@@ -51,29 +60,24 @@ int main()
 		numbers.push_back(num);
 	}
 
-	for (auto n : fav)
+	printList(fav, numbers, counter);
+
+	while(true)
 	{
-		std::cout << n << std::endl;
-	}
-	for (auto n : numbers)
-	{
-		switch (n)
+		int menuchoice = 0;
+		std::cout << "What would you like to do? Enter a number to decide\n"
+			<< "\t1. See list again\n\t2. Exit program\n";
+		getInt(menuchoice);
+		switch (menuchoice)
 		{
-		case 0:
-		case 5:
-		case 13:
-		case 19:
-		case 21:
-		case 36:
-		case 42:
-		case 50:
-		case 100:
-			std::cout << n << " was one of the special numbers!\n";
+		case 1:
+			printList(fav, numbers, counter);
 			break;
-		default:
-			std::cout << n << " was not special :(\n";
+		case 2:
+			return 0;
 			break;
 		}
 	}
+
 	return 0;
 }
---
+++ Labs/Lab02/Timer.h
@@ -0,0 +1,38 @@
+// Luke Underwood
+// 1/23/20
+// Lab02
+// Timer.h
+
+#ifndef TIMER_H
+#define TIMER_H
+
+#include<chrono>
+using namespace std::chrono;
+
+class Timer
+{
+public:
+	Timer();
+
+	Timer(const Timer& old);
+
+	Timer(time_point<system_clock> start);
+
+	~Timer();
+
+	void start();
+
+	void end();
+
+	double getDuration();
+
+private:
+	
+	double duration_;
+
+	time_point<system_clock> startTimepoint_;
+	time_point<system_clock> endTimepoint_;
+
+};
+
+#endif
