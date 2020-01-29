06b3c4d2dd2f2938314162087a06af6ad246ec13
luke.b.underwood@gmail.com <luke.b.underwood@gmail.com>
2020-01-28 17:05:54 -0900

    HW2 Spelunking - simple Cave member functions created

--- HW2/Spelunking/Cave.cpp
+++ HW2/Spelunking/Cave.cpp
@@ -4,4 +4,61 @@
 // Spelunking
 // Cave.cpp
 
+#include<iostream>
+
 #include "Cave.h"
+
+//Constructor automatically builds cave
+Cave::Cave()
+{
+
+}
+
+//Returns current room's number
+int Cave::getCurrentRoom()
+{
+	return currentRoom_;
+}
+
+//Changes position
+void Cave::goToRoom(int room)
+{
+	currentRoom_ = room;
+}
+
+//moves player to adjacent room 0, 1, or 2
+void Cave::goToAdjacentRoom(int room)
+{
+	currentRoom_ = caveRooms_[currentRoom_].adjacentRooms_[room];
+}
+
+//make two rooms adjacent
+void Cave::connect(int room1, int room2)
+{
+	caveRooms_[room1].adjacentRooms_.push_back(room2);
+	caveRooms_[room2].adjacentRooms_.push_back(room1);
+}
+
+//prints the short description
+void Cave::printShortDesc(int room) const
+{
+	std::cout << caveRooms_[room].shortDesc_;
+}
+
+//prints the long description
+void Cave::printLongDesc(int room) const
+{
+	std::cout << caveRooms_[room].longDesc_;
+}
+
+//save rooms to an output stream
+void Cave::saveRooms(std::ostream& os) const
+{
+
+}
+
+//read rooms from an input stream
+void Cave::readRooms(std::istream& is)
+{
+
+}
