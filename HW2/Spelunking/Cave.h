9f5e50d5f7f7ddc28258d4e4a8d4dc7e1be9f5e3
luke.b.underwood@gmail.com <luke.b.underwood@gmail.com>
2020-01-28 17:04:36 -0900

    HW2 Spelunking - Cave class prototypes and member variables in place

--- HW2/Spelunking/Cave.h
+++ HW2/Spelunking/Cave.h
@@ -4,13 +4,53 @@
 // Spelunking
 // Cave.h
 
-#include<>
+#include<fstream>
+#include<vector>
 
 class Cave
 {
 public:
 
+	//Constructor automatically builds cave
+	Cave();
+	
+	//Returns current room's number
+	int getCurrentRoom();
+	
+	//Changes position
+	void goToRoom(int room);
+	
+	//moves player to adjacent room 0, 1, or 2
+	void goToAdjacentRoom(int room);
+	
+	//make two rooms adjacent
+	void connect(int room1, int room2);
+	
+	//prints the short description
+	void printShortDesc(int room) const;
+	
+	//prints the long description
+	void printLongDesc(int room) const;
+	
+	//save rooms to an output stream
+	void saveRooms(std::ostream & os) const;
+	
+	//read rooms from an input stream
+	void readRooms(std::istream & is);
+	
 private:
-
+	struct Room
+	{
+		int roomNumber_;
+		std::string shortDesc_;
+		std::string longDesc_;
+		std::vector<int> adjacentRooms_;
+	
+		Room();
+	};
+	
+	int currentRoom_ = 0;
+	std::vector<Room> caveRooms_;
 };
+
 
