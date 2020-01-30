7fef35d921554a3b85eeb3b8dff3e251a9ee9a4f
luke.b.underwood@gmail.com <luke.b.underwood@gmail.com>
2020-01-30 10:42:56 -0900

    Lab03 complete

--- Labs/Lab03/Main.cpp
+++ Labs/Lab03/Main.cpp
@@ -10,17 +10,26 @@
 
 int main()
 {
+	std::cout << "Making raw pointer" << std::endl;
 	Timer* tPtr = new Timer;
 
 	time_point<system_clock> time = system_clock::now();
 
+	std::cout << "Making unique pointer" << std::endl;
 	auto uPtr1 = std::make_unique<Timer>(time);
+	{
+		std::cout << "Transferring ownership of unique pointer" << std::endl;
+		auto uPtr2 = std::move(uPtr1);
+	}
 
-	auto uPtr2 = std::move(uPtr1);
+	{
+		std::cout << "Making shared pointer" << std::endl;
+		auto sPtr1 = std::make_shared<Timer>(time);
 
-	(*uPtr2).end();
-
-	auto sPtr1 = std::make_shared<Timer>;
-
-	delete tPtr;
+		{
+			std::cout << "Creating a second shared pointer to the same object"
+				<< std::endl;
+			auto sPtr2 = sPtr1;
+		}
+	}
 }
