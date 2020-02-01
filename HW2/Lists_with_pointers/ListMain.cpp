9f2db379a0c9fddc9159fbe6bc96bf8790461c0d
luke.b.underwood@gmail.com <luke.b.underwood@gmail.com>
2020-01-31 17:25:40 -0900

    HW2 Lists - Project complete

--- HW2/Lists_with_pointers/ListMain.cpp
+++ HW2/Lists_with_pointers/ListMain.cpp
@@ -6,17 +6,60 @@
 
 #include<iostream>
 #include<list>
+#include<algorithm>
 
 #define CATCH_CONFIG_MAIN
 #include<catch2/catch.hpp>
 
-unsigned int Factorial(unsigned int number) {
-	return number <= 1 ? number : Factorial(number - 1) * number;
-}
-// wat
-TEST_CASE("Factorials are computed", "[factorial]") {
-	REQUIRE(Factorial(1) == 1);
-	REQUIRE(Factorial(2) == 2);
-	REQUIRE(Factorial(3) == 6);
-	REQUIRE(Factorial(10) == 3628800);
+struct Gods
+{
+	std::string hel = "hound";
+	std::string frigga = "owl";
+	std::string odin = "raven";
+	std::string loki = "serpent";
+	std::string thor = "sheep";
+	std::string sif = "swans";
+};
+
+TEST_CASE("Fifo, Lifo, Instert, and Sort", "[Linked Lists]") {
+	Gods gods;
+
+	std::list<std::string> og = { gods.hel, gods.frigga, gods.odin, gods.thor };
+	std::list<std::string> fifo = og;
+	std::list<std::string> lifo = og;
+
+	REQUIRE(og == fifo);
+	REQUIRE(og == lifo);
+
+	fifo.push_back(gods.sif);
+	lifo.push_back(gods.sif);
+
+	REQUIRE(fifo.back() == gods.sif);
+	REQUIRE(lifo.back() == gods.sif);
+
+	fifo.pop_front();
+	lifo.pop_back();
+
+	REQUIRE(fifo.front() == gods.frigga);
+	REQUIRE(fifo.back() == gods.sif);
+	REQUIRE(lifo == og);
+
+	std::list<std::string>::iterator searchTargetBegin = og.begin();
+	std::advance(searchTargetBegin, 2);
+	std::list<std::string>::iterator searchTargetEnd = searchTargetBegin;
+	searchTargetEnd++;
+	std::list<std::string>::iterator searchResult = og.end();
+	searchResult = std::search(og.begin(), og.end(), searchTargetBegin, searchTargetEnd);
+
+	REQUIRE(searchResult == searchTargetBegin);
+
+	og.insert(searchTargetEnd, gods.loki);
+	searchTargetEnd--;
+
+	REQUIRE(*searchTargetEnd == gods.loki);
+
+	for (auto n : og)
+	{
+		std::cout << n << std::endl;
+	}
 }
