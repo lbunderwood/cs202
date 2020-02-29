1f93015588db7a9af6f7cd16695c82de19abca67
luke.b.underwood@gmail.com <luke.b.underwood@gmail.com>
2020-02-29 11:35:57 -0900

    HW4 RecursionProblems - Factorial funcs complete

--- HW4/Recursion problems/RecursionFuncs.cpp
+++ HW4/Recursion problems/RecursionFuncs.cpp
@@ -4,32 +4,64 @@
 // Recursion Problems
 // RecursionFuncs.cpp
 
-unsigned long long int fib(int n)
-{
+#include<deque>
+#include<utility>
 
+// return term n of the fibonacci, recusrively defined
+unsigned long long int fib_slow(int n)
+{
+	// if fib(n-2) even exists, define recursively
+	if (n >= 2) return fib_slow(n - 1) + fib_slow(n - 2);
+
+	//else, return 1
+	else return 1;
 }
 
+// return term n of the fibonacci, recusrively defined, but the faster way
+std::pair<unsigned long long int, unsigned long long int> fib_fast(int n)
+{
+	// initialize some pairs to hold our stuff
+	auto previous = fib_fast(n - 1);
+
+	// if fib(n-2) even exists, define recursively
+	if (n >= 2)	return { previous.first + previous.second, previous.first };
+
+	//else, return 1
+	else return { 1, 1 };
+}
+
+// return term n of the fibonacci, not recursively defined
 unsigned long long int fib_loop(int n)
 {
+	// initialize deque with starting conditions
+	std::deque<unsigned long long int> seq = { 1, 1 };
 
+	// define recursively to index n
+	for (unsigned int i = 2; i <= n; i++)
+	{
+		seq.push_back(seq[i - 1] + seq[i - 2]);
+		seq.pop_front();
+	}
+
+	// return seq[n], not seq.back so that if we asked for 
+	// n = 0 or 1 we get the right thing
+	return seq.back();
 }
 
+// return n factorial, recursively defined
 unsigned long long int factorial(int n)
 {
 
 }
 
+// return n factorial, not recursively defined
 unsigned long long int factorial_loop(int n)
 {
 
 }
 
-unsigned long long int ack()
-{
-
-}
-
-unsigned long long int ack_loop()
+// return Ackerman's number m,n
+unsigned long long int ack(int m, int n)
 {
 
 }
--- HW4/Recursion problems/RecursionHead.h
+++ HW4/Recursion problems/RecursionHead.h
@@ -4,19 +4,27 @@
 // Recursion Problems
 // RecursionMain.cpp
 
+#include<utility>
+
 #ifndef RECURSIONHEAD_H
 #define RECURSIONHEAD_H
 
-unsigned long long int fib(int n);
+// return term n of the fibonacci, recusrively defined
+unsigned long long int fib_slow(int n);
 
+// return term n of the fibonacci, recusrively defined, but the faster way
+std::pair<unsigned long long int, unsigned long long int> fib_fast(int n);
+
+// return term n of the fibonacci, not recursively defined
 unsigned long long int fib_loop(int n);
 
+// return n factorial, recursively defined
 unsigned long long int factorial(int n);
 
+// return n factorial, not recursively defined
 unsigned long long int factorial_loop(int n);
 
-unsigned long long int ack();
-
-unsigned long long int ack_loop();
+// return Ackerman's number m,n
+unsigned long long int ack(int m, int n);
 
 #endif
