15b991be66157b7e39d43d68301534a34ee6d72c
luke.b.underwood@gmail.com <luke.b.underwood@gmail.com>
2020-04-13 20:06:15 -0800

    Idit4 - CityPath created

---
+++ Idit4/CityPath.cpp
@@ -0,0 +1,17 @@
+// Luke Underwood
+// 4/13/2020
+// cs202
+// TSPLIB 2
+// Iditarod #3
+// CityPath.cpp
+// CityPath class implementation
+
+#include "CityPath.h"
+
+CityPath::CityPath() {}
+
+CityPath::CityPath(std::vector<int> path)
+	: path_(path) {}
+
+CityPath::~CityPath() {}
+
---
+++ Idit4/CityPath.h
@@ -0,0 +1,28 @@
+// Luke Underwood
+// 4/13/2020
+// cs202
+// TSPLIB 2
+// Iditarod #3
+// CityPath.h
+// CityPath class header
+
+#ifndef CITYPATH_H
+#define CITYPATH_H
+
+#include<vector>
+
+class CityPath
+{
+public:
+
+	CityPath();
+	CityPath(std::vector<int> path);
+	~CityPath();
+
+private:
+
+	std::vector<int> path_
+
+};
+
+#endif
